6df42e93209ab6879fefac5ab43bd2b9d12e1d35
vishnusk <vishnuskumar95@gmail.com>
2017-04-12 12:19:16 +0530

    sas

---
+++ nn.sh
@@ -0,0 +1,11 @@
+qertyhbcb
+cndchecho "******************SALRY DETAILS***********************"
+echo "Enter Basic Pay"
+read bp
+hra=$((bp * 10/100 ))
+da=$((bp * 50/100))
+ta=250
+gp=$(($bp + $hra + $da +$ta))
+echo "*******************GROSS SALARY**********************"
+echo $gp
+
